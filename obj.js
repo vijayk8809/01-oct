b7582406484e6d2eff41dac339c00db5fbf79b61
vijayk8809 <130650711+vijayk8809@users.noreply.github.com>
2023-10-01 14:48:14 +0530

    first commit

---
+++ obj.js
@@ -0,0 +1,17 @@
+const classInfo = {
+    Vijay : {
+        class: "10",
+        city: "Ranchi",
+        grade: "A++"
+    },
+    Guddu : {
+        class: "11",
+        city: "Khunti",
+        grade: "A+"
+    },
+    Sachin : {
+        class: "Gradution",
+        city: "Gumla",
+        grade: "B+"
+    }
+};
